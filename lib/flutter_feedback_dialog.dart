f8c70a62084fd3850b77271425e1acd54381887a
subhadiprudra <neloy.nr2@gmail.com>
2025-06-15 12:39:59 +0530

    a

--- lib/flutter_feedback_dialog.dart
+++ lib/flutter_feedback_dialog.dart
@@ -1,2 +1,19 @@
 export 'package:flutter_feedback_dialog/main/flutter_feedback_dialog.dart';
 export 'package:flutter_feedback_dialog/models/feedback_models.dart';
+
+
+if(userSessions>x){
+  rating = showRating()
+  if(rating > 3) {
+    rated = askUserToRateOnAppStore/Playstore();
+    if(rated){
+      showThankYouMessage();
+      sendFeedbackToFeedbackNest()
+    } else {
+      x = x+ frequency
+    }
+  } else {
+    showFeedbackForm();
+    sendFeedbackToFeedbackNest()
+  }
+}
